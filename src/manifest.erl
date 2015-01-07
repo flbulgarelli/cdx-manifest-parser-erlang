55bb9259514330b9529106a50cb2f36d3fe499f5
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-07 13:19:15 -0300

    Exporting some functions

--- src/manifest.erl
+++ src/manifest.erl
@@ -1,12 +1,22 @@
 -module(manifest).
 
--export([parse_field_visibility/1, 
-  apply_field_mapping_to/2, 
+-export([
+  parse/1,
+  apply_to/2,
+  apply_mapping_to/2,
+  parse_field_visibility/1,
+  apply_field_mapping_to/2,
   extract_value/2]).
+
+parse(_ManifestJson) -> ok.
+
+apply_to(_Manifest, _Event) -> ok.
+
+apply_mapping_to(_Mapping, _Event) -> ok.
 
 
 parse_field_visibility(RawManifest) ->
-  case {lists:member({core, true}, RawManifest), 
+  case {lists:member({core, true}, RawManifest),
         lists:member({pii, true}, RawManifest),
         lists:member({indexed, true}, RawManifest)} of
     {true, true,  _   } -> pii;
@@ -20,15 +30,15 @@ parse_field_visibility(RawManifest) ->
 apply_field_mapping_to(FieldMapping, Event) ->
  ok.
 
-extract_value(Source, Event) -> ok. 
+extract_value(Source, Event) -> ok.
 
-  
+
 %% manifest: {manifest, Metadata, Mapping }
 %% Mapping: [ FieldMapping ]
 %% FieldMapping : { field_mapping, Target, Source, Signature }
 %% Signature : {Visibility, Type}
 %% Visibility : custom | indexed | pii
 %% Type : string | integer | boolean
-%% Source : 
+%% Source :
 
 %% field : { field, Name, Value, Signature  }
