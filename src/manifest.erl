2d4a0e5c0ed042aeae016d8b2e6edb45517f185f
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-08 14:51:58 -0300

    Refactor: removed duplications

--- src/manifest.erl
+++ src/manifest.erl
@@ -21,23 +21,24 @@ apply_field_mapping_to({field_mapping, Target, Source, Signature}, Event) ->
  {field, Target, extract_value(Source, Event), Signature }.
 
 extract_value({lookup, Path}, Event) ->
-  jsonpath:search(Path, Event);
-extract_value({beginning_of, Path, year}, Event) ->
-  {{Year, _, _}, _} = extract_date(Path, Event),
-  Year;
-extract_value({beginning_of, Path, month}, Event) ->
-  {{_, Month, _}, _} = extract_date(Path, Event),
-  Month;
-extract_value({strip, _, _}, Event) -> ok;
-extract_value({concat, _, _}, Event) -> ok;
-extract_value({substring, _, _}, Event) -> ok;
+  lookup_raw(Path, Event);
+extract_value({beginning_of, Path, TimeUnit}, Event) ->
+  beginning_of(lookup_date(Path, Event), TimeUnit);
+extract_value({strip, _, _}, _Event) -> ok;
+extract_value({concat, _, _}, _Event) -> ok;
+extract_value({substring, _, _}, _Event) -> ok;
 extract_value({milliseconds_between, _, _}, Event) -> ok;
-extract_value({convert_time, _, _}, Event) -> ok.
+extract_value({convert_time, _, _}, _Event) -> ok.
 
-extract_date(Path, Event) ->
-  DateString = binary:bin_to_list(jsonpath:search(Path, Event)),
-  ec_date:parse(DateString).
+lookup_raw(Path, Event)    ->
+  jsonpath:search(Path, Event).
+lookup_string(Path, Event) ->
+  binary:bin_to_list(lookup_raw(Path, Event)).
+lookup_date(Path, Event)   ->
+  ec_date:parse(lookup_string(Path, Event)).
 
+beginning_of({{_, Month, _}, _}, month) -> Month;
+beginning_of({{Year, _, _}, _},  year) -> Year.
 
 %% manifest: {manifest, Metadata, Mapping }
 %% Mapping: [ FieldMapping ]
